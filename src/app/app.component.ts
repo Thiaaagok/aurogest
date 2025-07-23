d00f9cf44e938ab2e0bf596017e68ac1e269d501
Thiago Valdez <thiago@kaitsoft.com.ar>
2025-07-23 14:51:19 -0300

    translation a español de primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ButtonModule} from 'primeng/button'
 import { NavbarComponent } from './features/common/components/navbar/navbar.component';
+import { PRIME_NG_CONFIG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
