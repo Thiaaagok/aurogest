a160e1675d0d0484e6f185f5f26345af4fe55888
sookins <thiago_valdez38@outlook.com>
2025-08-27 01:45:46 -0300

    login con usuario ejemplo

--- src/app/features/common/components/login/login.component.ts
+++ src/app/features/common/components/login/login.component.ts
@@ -9,23 +9,34 @@ import { Login } from '../../models/login.model';
 
 @Component({
   selector: 'app-login',
-  imports: [PrimeNgModule, CustomMaterialModule, CommonModule, RouterModule,FluidModule],
+  imports: [PrimeNgModule, CustomMaterialModule, CommonModule, RouterModule, FluidModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent { 
+export class LoginComponent {
 
   loginUsuario: Login = new Login();
 
   private utilitiesService = inject(UtilitiesService);
   private router = inject(Router);
 
-  constructor(){
+  constructor() {
     this.utilitiesService.setearLogin(true);
   }
 
-  login(){
+  login() {
+
+    const usuario = {
+      Id: 'd89da3c3-2687-4647-9b3e-17d6414115fd',
+      Usuario: 'Thiago',
+    };
+
+    localStorage.setItem('usuario', JSON.stringify({
+      Id: usuario.Id,
+      Usuario: usuario.Usuario,
+    }));
+
     this.router.navigateByUrl('home')
   }
-  
+
 }
